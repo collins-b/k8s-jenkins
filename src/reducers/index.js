65fcbbb906d670e3f26f050ba953ccd94d910b1e
Collins Wekesa <collins.wekesa@andela.com>
2017-06-25 23:38:17 +0300

    modify combined reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import newsReducer from './newsReducer';
 import newsSourceReducer from './newsSourceReducer';
 import techReducer from './techReducer';
+import politicsReducer from './politicsReducer';
 
  const rootReducer = combineReducers({
    news: newsReducer,
    sources: newsSourceReducer,
-   tech: techReducer
+   tech: techReducer,
+   politics: politicsReducer
  });
 
 export default rootReducer;
