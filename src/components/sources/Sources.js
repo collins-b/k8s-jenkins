9d1e815b8382b14099f91021ceefcf224b1487f5
Collins Wekesa <collins.wekesa@andela.com>
2017-06-25 17:03:40 +0300

    feat(sources): improve UI/UX

--- src/components/sources/Sources.js
+++ src/components/sources/Sources.js
@@ -3,11 +3,6 @@ import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import ContentAdd from 'material-ui/svg-icons/content/add';
-import TextField from'material-ui/TextField';
-import SelectField from 'material-ui/SelectField';
-import MenuItem from 'material-ui/MenuItem';
 import Gravatar from 'react-gravatar';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import Toggle from 'material-ui/Toggle';
@@ -31,7 +26,8 @@ constructor(props) {
       id: this.props.source.id,
       url: this.props.source.url,
       name: this.props.source.name,
-      category: this.props.source.category
+      category: this.props.source.category,
+      description: this.props.source.description
     }
     };
   }
@@ -41,33 +37,26 @@ constructor(props) {
  * @returns {component} component
  */
   render() {
-    console.log(this.props.source.id);
     return(
       <MuiThemeProvider>
-      <div className="row" style={{ marginLeft: 250,marginTop:1 }}>
-      <div className="col-lg-3 col-md-6" style={{ marginTop:75 }}>
-                  <div className="panel panel-green mdl-shadow--2dp">
-                    <div className="panel-heading">
-                      <div className="row">
-                        <div className="col-xs-3">
-                          <i className="fa fa-building-o fa-5x" />
-                        </div>
-                        <div className="col-xs-9 text-right">
-                          <div className="">{this.props.source.name}</div>
-                          <div>{this.props.source.category.capitalize()}</div>
-                        </div>
-                      </div>
-                    </div>
-                    <a href={this.props.source.url}>
-                      <div className="panel-footer">
-                        <span className="pull-left">Visit Source</span>
-                        <span className="pull-right"><i className="fa fa-arrow-circle-right" /></span>
-                        <div className="clearfix" />
-                      </div>
-                    </a>
-                  </div>
-                </div>
-      </div>
+      <div className="row" style={{ marginLeft: 250,marginTop:1 }}> 
+      <Card>
+    <CardHeader
+      title={this.props.source.name}
+      subtitle={this.props.source.category.capitalize()}
+    />
+    <CardText>
+     {this.props.source.description}
+    </CardText>
+    <CardActions>
+      <a href={this.props.source.url} target="_blank"><FlatButton label="Visit Source" /></a>
+      <FlatButton label="Share" />
+    </CardActions>
+  </Card>
+  <hr/>
+  </div>
+     
+
       </MuiThemeProvider>
     )
   }
